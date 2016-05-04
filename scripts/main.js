484c5c612e0fb384e34eec94fe7ba208af435ccb
Renato Ely <renato.silva@ccc.ufcg.edu.br>
2016-05-04 20:49:01 -0300

    Implementação parcial gráfico de linhas

--- scripts/main.js
+++ scripts/main.js
@@ -26,7 +26,7 @@
 function main() {
 
     var m = [20, 120, 20, 120],
-        w = 1280 - m[1] - m[3],
+        w = 2280 - m[1] - m[3],
         h = 900 - m[0] - m[2],
         i = 0,
         root = {};
@@ -68,6 +68,7 @@ function main() {
     var header2 = d3.select(document.getElementById("header2")); //Texto abaixo do anterior
     
     var toolTipAluno = d3.select(document.getElementById("toolTipAluno"));
+    var toolTipGrafLinhas = d3.select(document.getElementById("toolTipGraf"));
     
     //Grafico dos nós folhas
     var margin = {top: 10, right: 20, bottom: 30, left: 40};
@@ -103,6 +104,100 @@ var grafBarra = d3.select(document.getElementById("grafico")).append("svg")
 				.attr("height", height + margin.top + margin.bottom)
 				.append("g")
 				.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+				
+//Grafico de linhas
+function geraGrafico(){
+var margin = {top: 20, right: 80, bottom: 30, left: 50},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var parseDate = d3.time.format("%Y%m%d").parse;
+
+var x = d3.time.scale()
+    .range([0, width]);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var color = d3.scale.category10();
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
+
+var line = d3.svg.line()
+    .interpolate("basis")
+    .x(function(d) { return x(d.date); })
+    .y(function(d) { return y(d.temperature); });
+
+var svg = d3.select(document.getElementById("graficoLinhas")).append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.tsv("data/dados.tsv", function(error, data) {
+  if (error) throw error;
+
+  color.domain(d3.keys(data[0]).filter(function(key) { return key !== "date"; }));
+
+  data.forEach(function(d) {
+    d.date = parseDate(d.date);
+  });
+
+  var cities = color.domain().map(function(name) {
+    return {
+      name: name,
+      values: data.map(function(d) {
+        return {date: d.date, temperature: +d[name]};
+      })
+    };
+  });
+
+  x.domain(d3.extent(data, function(d) { return d.date; }));
+
+  y.domain([
+    d3.min(cities, function(c) { return d3.min(c.values, function(v) { return v.temperature; }); }),
+    d3.max(cities, function(c) { return d3.max(c.values, function(v) { return v.temperature; }); })
+  ]);
+
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Temperature (ºF)");
+
+  var city = svg.selectAll(".city")
+      .data(cities)
+    .enter().append("g")
+      .attr("class", "city");
+
+  city.append("path")
+      .attr("class", "line")
+      .attr("d", function(d) { return line(d.values); })
+      .style("stroke", function(d) { return color(d.name); });
+
+  city.append("text")
+      .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
+      .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.temperature) + ")"; })
+      .attr("x", 3)
+      .attr("dy", ".35em")
+      .text(function(d) { return d.name; });
+});
+		}
 				
     
 /*    var graph = new d3pie("pieChart", { //Grafico de pizza
@@ -304,6 +399,8 @@ var grafBarra = d3.select(document.getElementById("grafico")).append("svg")
            // sumNodes(root.children);
 	    setAnimacao(root.children);
             sumNodesCopia(root);
+            
+            geraGrafico();
         }
 
         function toggleAll(d) {
@@ -442,7 +539,7 @@ var grafBarra = d3.select(document.getElementById("grafico")).append("svg")
 
         // Normalize for fixed-depth.
         nodes.forEach(function (d) {
-            d.y = d.depth * 110;  //Diminuí o tamanho da perna de um nó para o outro (25/03/2016)
+            d.y = d.depth * 170;  //Diminuí o tamanho da perna de um nó para o outro (25/03/2016)
             d.numChildren = (d.children) ? d.children.length : 0;
 			var escala = d3.scale.linear().range(cores);
 			
@@ -523,7 +620,8 @@ var grafBarra = d3.select(document.getElementById("grafico")).append("svg")
 //            })
             .style("fill-opacity", ".8")
             .style("stroke", function (d) {
-				return d.linkColor;
+				return "#000000";
+				//return d.linkColor;
 //                return d.source ? d.source.linkColor : d.linkColor;
             });
 
@@ -560,7 +658,7 @@ var grafBarra = d3.select(document.getElementById("grafico")).append("svg")
             });
 
         nodeUpdate.select("circle")
-            .attr("r", raio)//function (d) { return isNaN(nodeRadius(d[spendField])) ? 2: nodeRadius(d[spendField]); })
+            .attr("r", raio+10)//function (d) { return isNaN(nodeRadius(d[spendField])) ? 2: nodeRadius(d[spendField]); })
             .style("fill", function (d) {
 				return d.linkColor;
 				})
@@ -681,97 +779,9 @@ var grafBarra = d3.select(document.getElementById("grafico")).append("svg")
             
             if (d.children || d._children){
 				if (detalhes){
-					
-					var margin = {top: 20, right: 80, bottom: 30, left: 50},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-
-var parseDate = d3.time.format("%Y%m%d").parse;
-
-var x = d3.time.scale()
-    .range([0, width]);
-
-var y = d3.scale.linear()
-    .range([height, 0]);
-
-var color = d3.scale.category10();
-
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
-
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left");
-
-var line = d3.svg.line()
-    .interpolate("basis")
-    .x(function(d) { return x(d.date); })
-    .y(function(d) { return y(d.temperature); });
-
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-d3.tsv("data/dados.tsv", function(error, data) {
-  if (error) throw error;
-
-  color.domain(d3.keys(data[0]).filter(function(key) { return key !== "date"; }));
-
-  data.forEach(function(d) {
-    d.date = parseDate(d.date);
-  });
-
-  var cities = color.domain().map(function(name) {
-    return {
-      name: name,
-      values: data.map(function(d) {
-        return {date: d.date, temperature: +d[name]};
-      })
-    };
-  });
-
-  x.domain(d3.extent(data, function(d) { return d.date; }));
-
-  y.domain([
-    d3.min(cities, function(c) { return d3.min(c.values, function(v) { return v.temperature; }); }),
-    d3.max(cities, function(c) { return d3.max(c.values, function(v) { return v.temperature; }); })
-  ]);
-
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
-
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Temperature (ºF)");
-
-  var city = svg.selectAll(".city")
-      .data(cities)
-    .enter().append("g")
-      .attr("class", "city");
-
-  city.append("path")
-      .attr("class", "line")
-      .attr("d", function(d) { return line(d.values); })
-      .style("stroke", function(d) { return color(d.name); });
-
-  city.append("text")
-      .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
-      .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.temperature) + ")"; })
-      .attr("x", 3)
-      .attr("dy", ".35em")
-      .text(function(d) { return d.name; });
-});
+					toolTipGrafLinhas.transition()
+					.duration(200)
+					.style("opacity", "2");
 				}
 				else{
 					
@@ -831,6 +841,9 @@ d3.tsv("data/dados.tsv", function(error, data) {
                 
             toolTipAluno.style("left", (d3.event.pageX + 15) + "px")
                 .style("top", (d3.event.pageY - 75) + "px");
+            
+            toolTipGrafLinhas.style("left", (d3.event.pageX + 15) + "px")
+                .style("top", (d3.event.pageY - 75) + "px");
 
             d3.select(labels[d.key]).transition().style("font-weight","bold").style("font-size","16");
 
@@ -869,6 +882,10 @@ d3.tsv("data/dados.tsv", function(error, data) {
                 .style("opacity", "0");
 			
 			toolTipAluno.transition()
+                .duration(500)
+                .style("opacity", "0");
+                
+            toolTipGrafLinhas.transition()
                 .duration(500)
                 .style("opacity", "0");
             
